4bfc0261e981dd7ac65adc01ff321f702beb741d
2766398 <2766398@learn.vsb.bc.ca>
2026-09-17 14:12:19 -0700

    Fix temperature conversion from Celsius to Fahrenheit

--- temp.java
+++ temp.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 	public class temp{
 		public static void main (String[] args){
-			final double C_TO_F = (9.0/5) + 32;
+			final double C_TO_F = (9.0/5);
 			double f;
 			Scanner as = new Scanner(System.in);
 			System.out.print("Enter a temperature in Celsius: ");
 			f = as.nextDouble();
-			double a = f * C_TO_F;
-			System.out.printf(f + " in Fahrenheit is %.1f F", f);
+			double a = f * C_TO_F +32;
+			System.out.printf(f + " C = %.1f F", a);
 	}
 }
